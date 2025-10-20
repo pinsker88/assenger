3bf771fa0a189657f78151798eafeba25a32d093
Natan Pinsker <npinsker@cloudlinux.com>
2025-10-21 00:55:15 +0400

    add servers

--- webapp/src/components/Page.vue
+++ webapp/src/components/Page.vue
@@ -70,15 +70,6 @@ const remoteSignal = ref("");  // JSON you paste in
 const rtcConfig = {
   iceServers: [
     // Works on LANs. For internet/NAT traversal, add a TURN server here.
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun.l.google.com:5349" },
-    { urls: "stun:stun1.l.google.com:3478" },
-    { urls: "stun:stun1.l.google.com:5349" },
-    { urls: "stun:stun2.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:5349" },
-    { urls: "stun:stun3.l.google.com:3478" },
-    { urls: "stun:stun3.l.google.com:5349" },
-    { urls: "stun:stun4.l.google.com:19302" },
     { urls: "stun:stun4.l.google.com:5349" }
   ],
 };
